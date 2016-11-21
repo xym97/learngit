49dfcdf8afb79610107a2500b0b8f73ab3e9e24d
xym97 <1179598879@qq.com>
2016-11-21 17:17:39 +0800

    2th my_strcmp

--- my_strcmp.c
+++ my_strcmp.c
@@ -1,28 +1,56 @@
 #include<stdio.h>
 #include<string.h>
 #define assert
-int main()
+//int main()
+//{   
+//	char dest[12];
+//	char src[] = "hello_bit";
+//	int i = 0;
+//	void my_strcmp(char* dest ,const char* src);
+//	my_strcmp(dest,src);
+//	printf("dest被cmp成了：%s",dest);
+//	return 0;
+//}
+//
+//  void my_strcmp(char* dest ,const char* src)
+// {
+//    assert(dest!=NULL);
+//	assert(src!= NULL);
+//	/*while(*src!='\0')
+//	{
+//       *dest = *src;
+//	   dest++;
+//	   src++;
+//	}*/
+//	while(*dest++ = *src++)
+//	{
+//         ;
+//	}
+//	//*dest = '\0';
+// }
+ int main()
 {   
 	char dest[12];
 	char src[] = "hello_bit";
 	int i = 0;
-	void my_strcmp(char* dest ,const char* src);
-	my_strcmp(dest,src);
-	printf("%s",dest);
+	int n = 5;
+	void my_strncmp(char* dest ,const char* src,int n);
+	my_strncmp(dest,src,n);
+	printf("dest被cmp成了：%s",dest);
 	return 0;
 }
 
-  void my_strcmp(char* dest ,const char* src)
+  void my_strncmp(char* dest ,const char* src,int n)
  {
     assert(dest!=NULL);
 	assert(src!= NULL);
-	/*while(*src!='\0')
+	/*while(*src!='\0'&&n--)
 	{
        *dest = *src;
 	   dest++;
 	   src++;
 	}*/
-	while(*dest++ = *src++)
+	while((*dest++ = *src++)&&--n)
 	{
          ;
 	}
