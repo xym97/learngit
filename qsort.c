b5fa382e6aef81b083993b021466ce09fa702719
xym97 <1179598879@qq.com>
2016-12-04 23:20:04 +0800

    EROOR CODE

--- qsort.c
+++ qsort.c
@@ -17,31 +17,44 @@ int intcmp(void* x,void* y)
 	int* _y = (int*)y;
 	return *_x - *_y;
 }
-void maopao(void* data,int num,int width,int(*cmp)(void*,void*))
+void maopao(void* data,int num,int width,int(*cmp)(void*,void*),void* dataend)
 {
-	//char* start = (char*)data;
-	int i = 0;
+	//int i = 0;
+	//assert(data);
+	//for(;i<num-1;i++){
+ //       int flag = 0;
+	//	int j = 0;
+	//    char* start = (char*)data;
+ //  		for(;j<num-1-i;j++){
+	//		if( (cmp(start,start+width))>0 ){
+	//			int k = 0;
+	//			flag = 1;
+ //  				for(;k<width;k++){
+	//			   start[k]^=start[k+width];
+	//			   start[k+width]^=start[k];
+	//			   start[k]^=start[k+width];
+	//			}
+	//			//flag = 1;
+ //  			}
+	//		start+=width;
+	//	}
+	//	if(!flag){break;}
+	//}
+	int pivotkey = 0;
+	int* start = (int*)data;
+    int* end = (int*)(dataend);
 	assert(data);
-	for(;i<num-1;i++){
-        int flag = 0;
-		int j = 0;
-	    char* start = (char*)data;
-   		for(;j<num-1-i;j++){
-			if( (cmp(start,start+width))>0 ){
-				int k = 0;
-				flag = 1;
-   				for(;k<width;k++){
-				   start[k]^=start[k+width];
-				   start[k+width]^=start[k];
-				   start[k]^=start[k+width];
-				}
-				//flag = 1;
-   			}
-			start+=width;
-		}
-		if(!flag){break;}
+	pivotkey = *start;
+	while(start<end){
+	     while(start<end && *end>=pivotkey)
+			 --end;
+		 *start = *end;
+	     while(start<end && *end<=pivotkey)
+			 ++start;
+		 *end = *start;
 	}
-	
+    //*start = *((int*)data);
+	//return strat
 }
 int main()
 {
@@ -49,7 +62,7 @@ int main()
 	int arr[] = {8,5,7,4,2,9,6,3,1}; 
 	int len = sizeof(arr)/sizeof(arr[0]);
 	showarr(arr,len);
-    maopao(arr,len,sizeof(arr[0]),intcmp);
+    maopao(arr,len,sizeof(arr[0]),intcmp,arr+len-1);
 	showarr(arr,len);
 	system("pause");
 	return 0;
