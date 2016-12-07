acfb41f3fbc059a02b2babd26838308789c688c5
xym97 <1179598879@qq.com>
2016-12-07 23:21:30 +0800

    ERROR CODE

--- qsort.c
+++ qsort.c
@@ -1,69 +1,117 @@
+//#include<stdio.h>
+//#include<assert.h>
+//#include<stdlib.h>
+//void showarr(int* arr,int len)
+//{
+//	int i = 0;
+//	assert(arr);
+//	for(;i<len;i++)
+//	{
+//        printf("%d ",arr[i]);
+//	}
+//	printf("\n");
+//}
+//int intcmp(void* x,void* y)
+//{
+//	int* _x = (int*)x;
+//	int* _y = (int*)y;
+//	return *_x - *_y;
+//}
+///* 这部分错误 可忽略
+////void maopao(void* data,int num,int width,int(*cmp)(void*,void*)/*void* dataend*/)
+////{
+////	int i = 0;
+////	assert(data);
+////	for(;i<num-1;i++){
+////        int flag = 0;
+////		int j = 0;
+////	    char* start = (char*)data;
+////   		for(;j<num-1-i;j++){
+////			if( (cmp(start,start+width))>0 ){
+////				int k = 0;
+////				flag = 1;
+////   				for(;k<width;k++){
+////				   start[k]^=start[k+width];
+////				   start[k+width]^=start[k];
+////				   start[k]^=start[k+width];
+////				}
+////				//flag = 1;
+////   			}
+////			start+=width;
+////		}
+////		if(!flag){break;}
+////	}
+////}
+////                 */
+//
+//int maopao(void* data,int num,int width,int(*cmp)(void*,void*)/*void* dataend*/)
+//{
+//	int pivotkey = 0;
+//	int* start = (int*)data;
+//	//int* end = (int*)dataend;
+//	int i = 0;
+//    int j = num - 1;
+//    //int* end = (int*)(dataend);
+//	assert(data);
+//	//start[0] = *start;
+//	pivotkey = *start;
+//	while(i<j){
+//	     while(i<j && start[j]>=pivotkey)
+//			 --j;
+//		 start[i] = start[j];
+//		 //start[j] ^= start[i];
+//		 //start[i] ^= start[j];
+//	     while(i<j && start[i]<=pivotkey)
+//			 ++i;
+//		 start[j] = start[i];
+//		//start[j] ^= start[i];
+//		// start[i] ^= start[j];
+//	}
+//    start[0] = pivotkey;
+//	//*start = start[0];
+//    //*start = *((int*)data);
+//	//return strat
+//}
+//int main()
+//{
+//	//int arr[] = {15,8,6,56,7,9,12,4,2,};
+//	int arr[] = {8,7,5,2,9,6,3,1,4}; 
+//	int len = sizeof(arr)/sizeof(arr[0]);
+//	showarr(arr,len);
+//    maopao(arr,len,sizeof(arr[0]),intcmp/*arr+len-1*/);
+//	showarr(arr,len);
+//	system("pause");
+//	return 0;
+//}
 #include<stdio.h>
-#include<assert.h>
 #include<stdlib.h>
-void showarr(int* arr,int len)
+#include<assert.h>
+#include<string.h>
+void stringshow(arr,len)
 {
 	int i = 0;
-	assert(arr);
-	for(;i<len;i++)
-	{
-        printf("%d ",arr[i]);
+	for(;i<len;i++){
+        printf("%s",arr[i]);	
 	}
 	printf("\n");
 }
-int intcmp(void* x,void* y)
+static int stringcmp(void* x,void* y)
 {
-	int* _x = (int*)x;
-	int* _y = (int*)y;
-	return *_x - *_y;
+	assert(x);assert(y);
+	return strcmp(*((char **)x),*((char **)y));
 }
-void maopao(void* data,int num,int width,int(*cmp)(void*,void*),void* dataend)
+stringqsort(char *arr[],int len)
 {
-	//int i = 0;
-	//assert(data);
-	//for(;i<num-1;i++){
- //       int flag = 0;
-	//	int j = 0;
-	//    char* start = (char*)data;
- //  		for(;j<num-1-i;j++){
-	//		if( (cmp(start,start+width))>0 ){
-	//			int k = 0;
-	//			flag = 1;
- //  				for(;k<width;k++){
-	//			   start[k]^=start[k+width];
-	//			   start[k+width]^=start[k];
-	//			   start[k]^=start[k+width];
-	//			}
-	//			//flag = 1;
- //  			}
-	//		start+=width;
-	//	}
-	//	if(!flag){break;}
-	//}
-	int pivotkey = 0;
-	int* start = (int*)data;
-    int* end = (int*)(dataend);
-	assert(data);
-	pivotkey = *start;
-	while(start<end){
-	     while(start<end && *end>=pivotkey)
-			 --end;
-		 *start = *end;
-	     while(start<end && *end<=pivotkey)
-			 ++start;
-		 *end = *start;
-	}
-    //*start = *((int*)data);
-	//return strat
+	assert(arr);
+	qsort(arr,len,sizeof(char*),stringcmp);
 }
 int main()
 {
-	//int arr[] = {15,8,6,56,7,9,12,4,2,};
-	int arr[] = {8,5,7,4,2,9,6,3,1}; 
+	char* arr[] = {"ddd","aaa","xxx","ggg","ppp"};
 	int len = sizeof(arr)/sizeof(arr[0]);
-	showarr(arr,len);
-    maopao(arr,len,sizeof(arr[0]),intcmp,arr+len-1);
-	showarr(arr,len);
+	stringshow(arr,len);
+	stringqsort(arr,len);
+	stringshow(arr,len);
 	system("pause");
 	return 0;
 }
